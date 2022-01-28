188c20d3bf3c375b91e7154aca7c23e8b0c1f250
Egemen <egemenwasd@gmail.com>
2022-01-28 16:22:59 +0300

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (x) {
+    console.log("original: ", x)
+    lowered = x.toLowerCase();
+    console.log("lowered: " ,lowered)
 
+    var purified = lowered.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\s]/gi, '');
+    console.log("purified: ", purified)
+
+    var array = []
+    for(i in purified) {
+    	array.push(purified[i])
+    };
+
+    console.log("array: ", array)
+    array.reverse()
+    reversed = array.join("")
+
+    if (reversed === purified) {
+        console.log("reversed: ",reversed)
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
